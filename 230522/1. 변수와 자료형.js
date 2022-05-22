4714161f90ad78847bdc0cdc566992a8730bb7b0
YeonJin Shin <83462564+Yeonjin-Shin@users.noreply.github.com>
2022-05-23 06:46:12 +0800

    Add files via upload

---
+++ 230522/1. 변수와 자료형.js
@@ -0,0 +1,24 @@
+var num = 1;
+
+var num = 10;
+
+console.log(num);
+
+let num2 = 2;
+/*let num2 = 20; 재선언 안됨*/
+num2 = 20; /*let 재할당 가능*/
+
+const num3 = 3;
+/*const num3 = 30; 재선언 안됨*/
+/*num3 = 30; 재할당 안됨*/
+
+//문자형 자료 선언
+
+let string = "안녕"
+
+//논리형 자료
+
+let bool = true;
+let t = 100>10;
+
+console.log(t);
